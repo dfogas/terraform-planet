f13c9db1502fb254593178ff6f7c7e3ea8d18f9b
david.fogas <david.fogas@ibillboard.com>
2020-09-26 13:19:47 +0200

    + add index.ts, change name in actions

--- domain/actions/actions.js
+++ domain/actions/actions.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.names = {
+exports.ACTION_NAMES = {
     PlaceTile: 'Place Tile',
 };
--- domain/actions/actions.ts
+++ domain/actions/actions.ts
@@ -1,4 +1,4 @@
-export const names = {
+export const ACTION_NAMES = {
     PlaceTile: 'Place Tile',
 
 };
